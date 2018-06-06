7404200152525d40522a22f80da1571e1f77ac55
Jacob Legg <skeletalsoup@protonmail.com>
2018-06-06 12:23:06 -0400

    this is my new file

---
+++ print_2_screen.rb
@@ -0,0 +1,24 @@
+#  p 1            
+#  puts 1
+#  print 1
+    # 100.times do 
+    #     p 1
+    # end
+    
+    number = 1
+    100.times do
+        if number == 3 
+         p "minds" 
+        else 
+            p number
+        end 
+    
+        p number
+        number = number + 1
+        
+    end
+
+    
+
+
+
